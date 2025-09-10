d69f2a999b350d7e0ccaa22f59a0620fc72b0585
Harry Jupiter <72943529+harryjupiter148@users.noreply.github.com>
2025-09-10 09:45:50 +0100

    Add files via upload

---
+++ users.js
@@ -0,0 +1,36 @@
+const name1 = document.getElementById("name1");
+const name2 = document.getElementById("name2");
+const name3 = document.getElementById("name3");
+const user1 = document.getElementById("user1");
+const user2 = document.getElementById("user2");
+const user3 = document.getElementById("user3");
+const mail1 = document.getElementById("mail1");
+const mail2 = document.getElementById("mail2");
+const mail3 = document.getElementById("mail3");
+
+async function fetchUserData() {
+  try {
+    let request = await fetch("https://jsonplaceholder.typicode.com/users");
+    let response = await request.json();
+    console.log(response);
+    let firstUser = response[0];
+    console.log(firstUser);
+    name1.textContent = firstUser.name;
+    user1.textContent = firstUser.username;
+    mail1.textContent = firstUser.email;
+    let seventhUser = response[7];
+    console.log(seventhUser);
+    name2.textContent = seventhUser.name;
+    user2.textContent = seventhUser.username;
+    mail2.textContent = seventhUser.email;
+    let lastUser = response[9];
+    console.log(lastUser);
+    name3.textContent = lastUser.name;
+    user3.textContent = lastUser.username;
+    mail3.textContent = lastUser.email;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchUserData();
